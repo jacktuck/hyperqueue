ea009472e79a5cf31b9cab36a544a1b8ff790bb9
Jack Tuck <jack.tuck@crugo.com>
2017-07-11 19:31:04 +0100

    Much faster here, i think .add is a big bottleneck for us here.

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
 const debug = require('debug')('runner')
-var Queue = require('bee-queue');
-var queue = new Queue('test');
+// var Queue = require('bee-queue')
+// var queue = new Queue('test')
+//
+//   var finished = 0
+//   var finishTime, startTime
+//
+//   var reportResult = function (result) {
+//     finished += 1
+//     if (finished === 1e4) {
+//       finishTime = (new Date()).getTime()
+//       debug('FINISHED IN', finishTime - startTime)
+//     }
+//   }
+//
+//   setTimeout(() => {
+//     for (var i = 0; i < 1e4; i++) {
+//       debug('adding')
+//       queue.createJob({i: i}).save()
+//     }
+//   }, 1000)
+//
+//
+//
+//   queue.process(5, function (job, done) {
+//     if (!startTime) startTime = (new Date()).getTime()
+//
+//     reportResult()
+//
+//     return done()
+//   })
 
-  var finished = 0;
-  var finishTime, startTime;
 
-  var reportResult = function (result) {
-    finished += 1;
-    if (finished === 1e4) {
-      finishTime = (new Date()).getTime();
-      debug('FINISHED IN', finishTime - startTime)
-    }
-  };
 
-  for (var i = 0; i < 1e4; i++) {
-    debug('adding')
-    queue.createJob({i: i}).save();
+const Queue = require('./lib/Queue').Queue
+const Redis = require('ioredis')
+
+var queue = new Queue('test', new Redis())
+
+var finished = 0
+var finishTime, startTime
+
+var reportResult = function (result) {
+  debug('finished', finished)
+  finished += 1
+  if (finished === 1e4) {
+    finishTime = (new Date()).getTime()
+    debug('FINISHED IN', finishTime - startTime)
   }
+}
 
-  startTime = (new Date()).getTime();
+queue.process(async function (job) {
+  if (!startTime) startTime = (new Date()).getTime()
 
-  queue.process(1, function (job, done) {
-    reportResult();
-    return done();
-  });
+  reportResult()
+})
 
-
-
-// const Queue = require('./lib/Queue').Queue
-// const Redis = require('ioredis')
-// const debug = require('debug')('runner')
-//
-// var queue = new Queue('test', new Redis())
-//
-// var finished = 0
-// var finishTime, startTime
-//
-// var reportResult = function (result) {
-//   debug('finished', finished)
-//   finished += 1
-//   if (finished === 1e4) {
-//     finishTime = (new Date()).getTime()
-//     debug('FINISHED IN', finishTime - startTime)
-//   }
-// }
-//
-// queue.process(async function (job) {
-//   reportResult()
-// })
-//
-// startTime = (new Date()).getTime()
-//
-// setTimeout(() => {
-//   for (var i = 0; i < 1e4; i++) {
-//     // debug('adding')
-//     queue.add({i: i})
-//   }
-// }, 1000)
+setTimeout(() => {
+  for (var i = 0; i < 1e4; i++) {
+    queue.add({i: i})
+  }
+}, 1000)
