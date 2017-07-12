a1c7a4652ecf1cf957f396d19f90e8f0c639af8c
Jack Tuck <jack-tuck@hotmail.com>
2017-07-12 02:11:14 +0100

    WIP

---
+++ benchmark/bee-queue.js
@@ -0,0 +1,66 @@
+let usage = require('usage')
+let debug = require('debug')('bee-queue')
+
+let Queue = require('bee-queue')
+let queue = new Queue('test')
+
+let finished = ran = 0
+let finishTime, startTime
+
+let concurrency = parseInt(process.env.CONCURRENCY)
+let jobs = parseInt(process.env.JOBS)
+let runs = parseInt(process.env.RUNS) || 1
+
+;(async () => {
+  let results = []
+
+  let makeLog = () => usage.lookup(process.pid, (err, usage) => {
+    if (err) debug('err', err)
+
+    let o = {}
+
+    o.concurrency = concurrency
+    o.jobs = jobs
+    o.runs = runs
+    o.duration = results.reduce((sum, val) => sum += val, 0) + ' ms'
+    o.times = results.map(v => v + ' ms')
+    o.avg = results.reduce((sum, val) => sum += val, 0) / runs + ' ms'
+    o.usage = usage
+
+    debug('~~ bee-queue benchmark ~~')
+    debug(JSON.stringify(o, null, 4))
+  })
+  var addSomejobs = function () {
+    // debug('adding some jobs')
+    for (var i = 0; i < jobs; i++) {
+      queue.createJob({i: i}).save()
+    }
+  }
+
+  var reportResult = async function (result) {
+    finished += 1
+
+    // debug(finished, '/', jobs)
+
+    if (finished === jobs) {
+      finishTime = (new Date()).getTime()
+
+      results.push(finishTime - startTime)
+
+      if (++ran < runs) addSomejobs()
+      else makeLog()
+
+      finished = 0
+      startTime = (new Date()).getTime()
+    }
+  }
+
+  queue.process(concurrency, function (job, done) {
+    reportResult()
+    return done()
+  })
+
+  startTime = (new Date()).getTime()
+
+  addSomejobs()
+})().catch(debug)
---
+++ benchmark/meeseek.js
@@ -0,0 +1,65 @@
+let usage = require('usage')
+let debug = require('debug')('meeseek')
+
+let Queue = require('../lib/Queue')
+let Redis = require('ioredis')
+let queue = new Queue('test', new Redis())
+
+let finished = ran = 0
+let finishTime, startTime
+
+let jobs = parseInt(process.env.JOBS)
+let runs = parseInt(process.env.RUNS) || 1
+
+;(async () => {
+  let results = []
+
+  let makeLog = () => usage.lookup(process.pid, (err, usage) => {
+    if (err) debug('err', err)
+
+    let o = {}
+
+    o.jobs = jobs
+    o.runs = runs
+    o.duration = results.reduce((sum, val) => sum += val, 0) + ' ms'
+    o.times = results.map(v => v + ' ms')
+    o.avg = results.reduce((sum, val) => sum += val, 0) / runs + ' ms'
+    o.usage = usage
+
+    debug('~~ meeseek benchmark ~~')
+    debug(JSON.stringify(o, null, 4))
+  })
+
+  var addSomejobs = function () {
+    // debug('adding some jobs')
+    for (var i = 0; i < jobs; i++) {
+      queue.add({i: i})
+    }
+  }
+
+  var reportResult = function (result) {
+    finished += 1
+
+    // debug(finished, '/', jobs)
+
+    if (finished === jobs) {
+      finishTime = (new Date()).getTime()
+
+      results.push(finishTime - startTime)
+
+      if (++ran < runs) addSomejobs()
+      else makeLog()
+
+      finished = 0
+      startTime = (new Date()).getTime()
+    }
+  }
+
+  await queue.process(async function (job, done) {
+    reportResult()
+  })
+
+  startTime = (new Date()).getTime()
+
+  addSomejobs()
+})().catch(debug)
